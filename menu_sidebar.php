e501fd188959f4360b2c5e55c66ff966aef7857e
Renato Jozinovic <renato.jozinovic@skole.hr>
2017-02-13 12:08:31 +0100

    promijenili smo naziv stranice

--- menu_sidebar.php
+++ menu_sidebar.php
@@ -2,10 +2,10 @@
               <div class="menu_section">
                 <h3>Glavni izbornik</h3>
                 <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Početna <span class="fa fa-chevron-down"></span></a>
+                  <li><a><i class="fa fa-car"></i> Proizvođači <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                      <li><a href="index.html">Dashboard</a></li>
-                      <li><a href="index2.html">Dashboard2</a></li>
+                      <li><a href="brands.php">Prikaz</a></li>
+                      <li><a href="brands_new.php">Unos</a></li>
                     </ul>
                   </li>
                   <li><a><i class="fa fa-edit"></i> Forme <span class="fa fa-chevron-down"></span></a>

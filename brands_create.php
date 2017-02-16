6a45c50d0c195418b72fe85004b14b3428ad7a89
Renato Jozinovic <renato.jozinovic@skole.hr>
2017-02-16 11:21:40 +0100

    Uspjeli smo napraviti unos proizvođača

---
+++ brands_create.php
@@ -0,0 +1,10 @@
+<?php require('connect.php'); ?>
+
+<?php
+
+$sql = "INSERT INTO brands (name, year_est) VALUES ('" . $_POST['name'] . "', '" . $_POST['year_est'] . "');";
+$mysqli->query($sql);
+
+header('Location: brands.php');
+
+?>
